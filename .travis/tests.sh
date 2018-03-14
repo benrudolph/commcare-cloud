7fbb9c8ba9fe995f6096277f07df705e9379e506
Daniel Roberts <droberts@dimagi.com>
2018-03-14 17:54:15 -0400

    set up .travis.yml for running a test on the side
    that does't affect build outcome

--- .travis/tests.sh
+++ .travis/tests.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-test_syntax() {
-  ansible-playbook -i ansible/inventories/test ansible/deploy_stack.yml --syntax-check
-}
+if [[ ${TEST} = 'main' ]]
+then
 
-test_localsettings() {
-  ansible-playbook -i ansible/inventories/test ansible/deploy_stack.yml -e '@ansible/vars/dev/dev_private.yml' -e '@ansible/vars/dev/dev_public.yml' --tags=commcarehq
-  sudo python -m py_compile /home/cchq/www/dev/current/localsettings.py
-}
+    test_syntax() {
+        ansible-playbook -i ansible/inventories/test ansible/deploy_stack.yml --syntax-check
+    }
 
-test_syntax
-test_localsettings
+    test_localsettings() {
+        ansible-playbook -i ansible/inventories/test ansible/deploy_stack.yml -e '@ansible/vars/dev/dev_private.yml' -e '@ansible/vars/dev/dev_public.yml' --tags=commcarehq
+        sudo python -m py_compile /home/cchq/www/dev/current/localsettings.py
+    }
+
+    test_help_cache() {
+        diff <(ansible -h) commcare-cloud/commcare_cloud/help_cache/ansible.txt
+        diff <(ansible-playbook -h) commcare-cloud/commcare_cloud/help_cache/ansible-playbook.txt
+    }
+
+    test_syntax
+    test_localsettings
+    test_help_cache
+    nosetests
+
+elif [[ ${TEST} = 'prove-deploy' ]]
+then
+    fail() {
+        [[ 'a' = 'b' ]]
+    }
+fi
